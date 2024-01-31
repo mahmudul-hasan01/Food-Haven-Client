45d32b6784f186e84d3f8a6deea5b940fe724717
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-01-31 19:44:42 +0600

    add footer

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Page/Shared/Footer";
 
 const MainLayout = () => {
     return (
         <div>
-            
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            
+            <img className="w-20 h-20" src="../../../public/chef-hat-with-spoon-fork_602006-247.avif" alt="" />
         </div>
     );
 };
