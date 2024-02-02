0f5c3df1c5fa53cdf898f15e5048f90d486aed78
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-02-02 20:55:26 +0600

    use react tab

--- src/Page/Order/Order.jsx
+++ src/Page/Order/Order.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import orderImg from '../../assets/shop/banner2.jpg'
 import Cover from '../Shared/Cover';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Order = () => {
+    const [tabIndex,setTabIndex] = useState(0)
     return (
         <div>
             <Cover img={orderImg} title={'Order Food'}></Cover>
-           
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
