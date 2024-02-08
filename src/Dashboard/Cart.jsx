10143a1925eeeda187173dd0a6e450b89ae9507b
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-02-08 18:24:33 +0600

    show my cart data and delete inside dashboard

--- src/Dashboard/Cart.jsx
+++ src/Dashboard/Cart.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import useCart from '../Hooks/useCart';
+import { MdDeleteForever } from "react-icons/md";
+import useAxiosSecure from '../Hooks/useAxiosSecure';
 
 const Cart = () => {
+    const { cart, refetch } = useCart()
+    const axiosSecure = useAxiosSecure()
+
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const handleDelete = async (id) => {
+        await axiosSecure.delete(`/carts/${id}`)
+        refetch()
+
+    }
     return (
         <div>
-            <h1 className="">cart</h1>
+            <div className="flex justify-between">
+                <h1 className="uppercase text-3xl">total order: {cart.length}</h1>
+                <h1 className="uppercase  text-3xl">total price: {totalPrice}</h1>
+                <button className='btn btn-info'>Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead className='text-blue-800'>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <th>{index + 1}</th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item?.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="font-bold">{item?.name}</div>
+                                </td>
+                                <td className="font-bold">$ {item?.price}</td>
+                                <th>
+                                    <button onClick={() => handleDelete(item?._id)}><MdDeleteForever className='text-3xl' /></button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+
+
+                </table>
+            </div>
         </div>
     );
 };
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,20 +1,26 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaAddressCard, FaCalendar, FaHome, FaList } from "react-icons/fa";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import useCart from "../Hooks/useCart";
 
 const Dashboard = () => {
+    const {cart} = useCart()
     return (
         <div className="flex">
-            <div className="w-64 min-h-screen bg-cyan-100">
+            <div className="w-64 min-h-screen bg-red-200">
                 <ul className="menu space-y-5 mt-6">
-                    <li><NavLink to='/dashboard/cart'><FaCartShopping /> My Cart</NavLink></li>
+                    <li><NavLink to='/dashboard/cart'><FaCartShopping /> My Cart ({cart?.length})</NavLink></li>
                     <li><NavLink to='/dashboard/userHome'><FaHome /> User Home</NavLink></li>
                     <li><NavLink to='/dashboard/reservation'><FaCalendar /> Reservation</NavLink></li>
                     <li><NavLink to='/dashboard/review'><FaAddressCard /> Review</NavLink></li>
                     <li><NavLink to='/dashboard/booking'><FaList /> My Booking</NavLink></li>
+                    <div className="divider"></div>
+                    <li><NavLink to='/'><FaHome /> Home</NavLink></li>
+                    <li><NavLink to='/menu'><MdOutlineRestaurantMenu /> Menu</NavLink></li>
                 </ul>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
