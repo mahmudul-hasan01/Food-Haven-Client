3ab3886fc54c8619a800400012eb8cfbafd063d7
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-02-12 12:06:18 +0600

    verify api using jwt token

--- src/Components/Providers/AuthProviders.jsx
+++ src/Components/Providers/AuthProviders.jsx
@@ -1,12 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { auth } from "../FirebaseConfig/Firebase.config";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 
 
 export const AuthContext = createContext(null)
 const AuthProviders = ({ children }) => {
 
 
+    const axiosPublic = useAxiosPublic()
     const googleProvider = new GoogleAuthProvider()
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
@@ -41,7 +43,18 @@ const AuthProviders = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
-            console.log(currentUser);
+            if(currentUser){
+                const userInfo = {email: currentUser?.email}
+                axiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token)
+                    }
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
             setLoading(false)
         })
         return () => {
